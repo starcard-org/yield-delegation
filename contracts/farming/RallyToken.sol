dc668544d4dfd4860d61ac71ca6ccc5acaf90a03
amit-rally <48840260+amit-rally@users.noreply.github.com>
2020-09-24 19:52:58 -0700

    liquidity mining
    based on sushi but rewards come from a balance of reward tokens allocated to the pool instead of allowing the pool to mint new reward tokens

---
+++ contracts/farming/RallyToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RallyToken is ERC20 {
+
+    //15 billion fixed token supply with default 18 decimals
+    uint256 public constant TOKEN_SUPPLY = 15 * 10**9 * 10**18;
+
+    constructor (
+        address _escrow
+    ) public ERC20(
+	"Rally Network Token",
+	"RLY"
+    ) {
+        _mint(_escrow, TOKEN_SUPPLY);	
+    }
+
+
+}
