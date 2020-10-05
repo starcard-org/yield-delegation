cce750ee62cdfe986cdeb87168ff690f3db0945d
amit-rally <48840260+amit-rally@users.noreply.github.com>
2020-10-04 22:17:53 -0700

    add rewards distributor
    create a single rewards distributor responsible for the entire pool of rewards. individual ydvs withdraw from this pool when allocating reward tokens. additionally, distributor serves as a single contract with which all YDVs capable of distributing rewards are registered

--- migrations/4_deploy_ydvs.js
+++ migrations/4_deploy_ydvs.js
@@ -1,7 +1,10 @@
 const YieldDelegatingVault = artifacts.require("YieldDelegatingVault");
 const RallyToken = artifacts.require("RallyToken");
+const YDVRewardsDistributor = artifacts.require("YDVRewardsDistributor");
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network, accounts) {
+  await deployer.deploy(YDVRewardsDistributor, RallyToken.address);
+
   let controller = process.env.CONTROLLER;
   let treasury = process.env.TREASURY;
   let yusdc_address = "0x597ad1e0c13bfe8025993d9e79c69e1c0233522e"; //yearn usdc vault
@@ -9,14 +12,17 @@ module.exports = async function(deployer) {
   console.log("controller = " + controller);
   console.log("treasury = " + treasury);
 
-  await deployer.deploy(
+  let ydv = await deployer.deploy(
     YieldDelegatingVault,
     controller,
     yusdc_address,
-    RallyToken.address,
+    YDVRewardsDistributor.address,
     treasury,
     "9000", //out of 10000
     "100000000000000", //global deposit cap
     "100000000000" //per deposit cap
   ); //proxy into yearn USDC vault
+
+  let rewards = new web3.eth.Contract(YDVRewardsDistributor.abi, YDVRewardsDistributor.address);
+  rewards.methods.addYDV(ydv.address).send({from:accounts[0]});
 };
