e2b9200450e782a873b145bfdda002373f0a6f49
amit-rally <48840260+amit-rally@users.noreply.github.com>
2020-10-08 20:18:14 -0700

    remove unused files

--- contracts/interfaces/ControllerInterface.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.6.2;
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-interface ControllerInterface {
-    function payReward(address, uint256) external;
-    function setYDV(address, address) external;
-}
