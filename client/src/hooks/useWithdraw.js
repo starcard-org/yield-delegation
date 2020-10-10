b8ed242cb333d4221d4bd5ced4d1d9e29d3b821a
Alexandre Clement <aclement@rally.io>
2020-10-10 12:22:52 -0400

    Fix withdraw to use withdrawyToken when needed

--- client/src/hooks/useWithdraw.js
+++ client/src/hooks/useWithdraw.js
@@ -7,7 +7,7 @@ export const useWithdraw = name => {
   const {drizzle, initialized} = useContext(DrizzleContext);
   const {account} = useWallet();
   const withdraw = useCallback(
-    async (amount, decimals) => {
+    async (amount, decimals, method = 'withdraw') => {
       if (!drizzle || !initialized) {
         return;
       }
@@ -15,12 +15,10 @@ export const useWithdraw = name => {
       // If ever we still have decimals after applying the decimals
       // we need to get rid of them.
       const value = decToBn(amount, decimals).toFixed(0, 1);
-      console.log(value);
-      const tx = await drizzle.contracts[name].methods.withdraw(value.toString()).send({
+      const tx = await drizzle.contracts[name].methods[method](value.toString()).send({
         from: account,
       });
 
-      console.log(tx);
       return tx.status;
     },
     [account, name, drizzle, initialized]
