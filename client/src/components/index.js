4114945988aca64e27140eb61cafaad5101a56e4
Alexandre Clement <aclement@rally.io>
2020-10-16 09:25:04 -0400

    Moved client code to it's own repo

--- client/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export {Card, CardContent, CardIcon, CardTitle} from './Card';
-export {Header} from './Header';
-export {Footer} from './Footer';
-export {Container} from './Container';
-export {Button} from './Button';
-export {Accordion} from './Accordion';
-export {ConnectYourWallet} from './ConnectYourWallet';
--- client/src/utils/number.js
+++
@@ -1,9 +0,0 @@
-import BigNumber from 'bignumber.js';
-
-export const bnToDec = (bn, decimals = 18) => {
-  return bn.dividedBy(new BigNumber(10).pow(decimals)).toNumber();
-};
-
-export const decToBn = (dec, decimals = 18) => {
-  return new BigNumber(dec).multipliedBy(new BigNumber(10).pow(decimals));
-};
